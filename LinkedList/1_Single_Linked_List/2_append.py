1b31712902e7a74cc73af80275865aa95717e0b1
Anmol Sirola <anmolsirola57@gmail.com>
2025-11-06 17:33:28 +0000

    Added functions

--- LinkedList/1_Single_Linked_List/2_append.py
+++ LinkedList/1_Single_Linked_List/2_append.py
@@ -15,8 +15,24 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
 
 my_linked_list = LinkedList(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
 my_linked_list.print_list()
 
 
